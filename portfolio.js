b32b7c1d28224725bb2a8f8cf84dda0dad4ab844
Jonathan Jackson <jacksonj9891@gmail.com>
2022-12-17 21:29:38 -0500

    Added project and restyled

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,21 @@
-const nav = document.getElementById('navbar');
-document.addEventListener('scroll', (e) => {
-    if (window.pageYOffset > 0) {
-      nav.style.backgroundColor = 'rgb(252, 163, 17)';
-      nav.style.fontSize = '1.1em';
-    } else {
-      nav.style.backgroundColor = '';
-      nav.style.fontSize = '1em';
-    }
-  })
+const nav = document.getElementById("navbar");
+document.addEventListener("scroll", (e) => {
+  if (window.pageYOffset > 0) {
+    nav.style.backgroundColor = "rgb(252, 163, 17)";
+    nav.style.fontSize = "1.1em";
+  } else {
+    nav.style.backgroundColor = "";
+    nav.style.fontSize = "1em";
+  }
+});
+
+function naming(element) {
+  const overlay = element.previousElementSibling.childNodes[3];
+  if (overlay.classList.contains("hidden")) {
+    overlay.classList.remove("hidden");
+    element.innerHTML = "View Less";
+  } else {
+    overlay.classList.add("hidden");
+    element.innerHTML = "View More";
+  }
+}
